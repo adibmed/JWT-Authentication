f62a7b11ebbb686dc1214be9ea033bedc0feb546
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-26 23:42:03 +0000

    Config JWT && Return Secret to User

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 
-console.log('starting...')
+console.log("starting...");
 
-app.get("/", (req, res) => {
- res.send('hello World')
+app.get("/posts", (req, res) => {
+  res.send("hello World");
+});
+
+app.post("/login", (req, res) => {
+    console.log('req 👉 ', req.body)
+  // Athenticate User
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+  res.json({ accessToken: accessToken });
 });
 
 app.listen(3005);
