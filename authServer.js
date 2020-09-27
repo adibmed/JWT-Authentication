292b956ac4eec6dbb94b542760df35640ed85ad0
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-27 01:26:47 +0000

    Unregister Token [delete]

--- authServer.js
+++ authServer.js
@@ -4,7 +4,26 @@ const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
 
-app.use(express.json())
+app.use(express.json());
+
+// Local token [not for production]
+let refreshTokens = [];
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
+  jwt.verify(refreshToken, process.env.REFREASH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accesstoken = generateAccessToken({ name: user.name });
+    res.json({ accessToken: accessToken });
+  });
+});
+
+app.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+});
 
 app.post("/login", (req, res) => {
   // Athenticate User
